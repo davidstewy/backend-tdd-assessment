4dcd6736d5cb2c8b04d7f200ad3cabc0dd1f852b
David Stewart <davidstewy@gmail.com>
2018-12-12 13:49:21 -0500

    Functionality working

--- echo.py
+++ echo.py
@@ -2,21 +2,40 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "davidstewy"
 
 
 import sys
+import argparse
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+    # Create and define your parser engine here, but DONT PARSE.
+    parser = argparse.ArgumentParser(
+        description="Perform transformation on input text.")
+    parser.add_argument("text", help="text to be manipulated")
+    parser.add_argument(
+        "-u", "--upper", action="store_true", help="convert text to uppercase")
+    parser.add_argument(
+        "-l", "--lower", action="store_true", help="convert text to lowercase")
+    parser.add_argument(
+        "-t", "--title", action="store_true", help="convert text to titlecase")
+    return parser
 
 
 def main(args):
-    """Implementation of echo"""
-    pass
+    parser = create_parser()
+    namespace = parser.parse_args(args)
+    text = namespace.text
+    if namespace.upper:
+        text = text.upper()
+    if namespace.lower:
+        text = text.lower()
+    if namespace.title:
+        text = text.title()
+    return text
 
 
 if __name__ == '__main__':
-    pass
+    print main(sys.argv[1:])
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -3,8 +3,72 @@
 
 import unittest
 import echo
+import subprocess
 
-# Your test case class goes here
+
+class TestEcho(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = echo.create_parser()
+
+    def test_help(self):
+        """ Running the program without arguments should show usage. """
+
+        # Run the command `python ./echo.py -h` in a separate process, then
+        # collect it's output.
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        usage = open("./USAGE", "r").read()
+        self.assertEquals(stdout, usage)
+
+    def test_upper_short(self):
+        arg_list = ["-u", "hello"]
+        namespace = self.parser.parse_args(arg_list)
+        # W as the '-u' attr
+        self.assertTrue(namespace.upper)
+        # Did the program transform our text to uppercase?
+        self.assertEquals(echo.main(arg_list), "HELLO")
+
+    def test_upper_long(self):
+        arg_list = ["--upper", "hello"]
+        namespace = self.parser.parse_args(arg_list)
+        # W as the '-u' attr
+        self.assertTrue(namespace.upper)
+        # Did the program transform our text to uppercase?
+        self.assertEquals(echo.main(arg_list), "HELLO")
+
+    def test_lower_short(self):
+        arg_list = ["-l", "HELLO"]
+        namespace = self.parser.parse_args(arg_list)
+        self.assertTrue(namespace.lower)
+        self.assertEquals(echo.main(arg_list), 'hello')
+
+    def test_lower_long(self):
+        arg_list = ["--lower", "HELLO"]
+        namespace = self.parser.parse_args(arg_list)
+        self.assertTrue(namespace.lower)
+        self.assertEquals(echo.main(arg_list), 'hello')
+
+    def test_title_short(self):
+        arg_list = ["-t", "hello"]
+        namespace = self.parser.parse_args(arg_list)
+        self.assertTrue(namespace.title)
+        self.assertEquals(echo.main(arg_list), 'Hello')
+
+    def test_title_long(self):
+        arg_list = ["--title", "hello"]
+        namespace = self.parser.parse_args(arg_list)
+        self.assertTrue(namespace.title)
+        self.assertEquals(echo.main(arg_list), 'Hello')
+
+    def test_all_options(self):
+        arg_list = ["-tul", 'hElLo']
+        namespace = self.parser.parse_args(arg_list)
+        self.assertTrue(all([namespace.lower,
+                             namespace.upper, namespace.title]))
+        self.assertEquals(echo.main(arg_list), 'Hello')
 
 
 if __name__ == '__main__':
